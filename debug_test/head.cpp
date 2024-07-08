5a6f53e09440eb7067e6294130f533ffb2afcfd1
Felix_Xie <146699316+Xtycs@users.noreply.github.com>
2024-07-08 23:58:27 +0800

    Add files via upload

---
+++ debug_test/head.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// int test()
+// {
+//     printf("Function is called\n");
+//     return 0;
+// }
+
+static int g_val = 2030;
+
+static int Add(int a, int b)
+{
+    return a + b;
+}
